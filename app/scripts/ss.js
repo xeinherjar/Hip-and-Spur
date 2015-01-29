ca9e296f54e37d7de3cfcda08961405f29e6122c
Stephen Settle <ssettle3@gmail.com>
2015-01-28 22:12:46 -0500

    Added workable objects for menu, news, and specials

--- app/scripts/ss.js
+++ app/scripts/ss.js
@@ -1,10 +1,34 @@
-var menuItems, data
+var menuItems, news, specials
 
-a = $.getJSON('http://private-anon-e6abf6810-restaurantapi.apiary-mock.com/menu')
+
+//Menu
+
+$.getJSON('http://private-anon-e6abf6810-restaurantapi.apiary-mock.com/menu')
 	.done(function(data){
-		console.log('Sucess')
+		menu = data;
+		entrees = menu.entrees;
+		sides = menu.sides;
+		app = menu.appetizers;
 	}) .fail( function (){
 		console.log('Fail')
 	});
 
-menuItems = a.responseJSON
+//News
+
+$.getJSON('http://private-anon-e6abf6810-restaurantapi.apiary-mock.com/news/latest')
+	.done(function(data){
+		news = data;
+	}) .fail( function (){
+		console.log('Fail')
+	});
+
+
+
+//Daily Specials
+
+$.getJSON('http://private-anon-e6abf6810-restaurantapi.apiary-mock.com/menu/special')
+	.done(function(data){
+		specials = data;
+	}) .fail( function (){
+		console.log('Fail')
+	});
