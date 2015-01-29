231778eaa4b3526d5c09ee44856cf539e55748c4
Alex Pate <proto.merkabah@gmail.com>
2015-01-29 11:46:14 -0500

    Start of ig-img style, append photos to page

--- app/scripts/ap.js
+++ app/scripts/ap.js
@@ -1,16 +1,28 @@
 var endpoint = 'https://api.instagram.com/v1/'
-var food_search = 'tags/food/media/recent?client_id='
+var food_search = 'tags/steak/media/recent?client_id='
 
 var search_url = endpoint + food_search + client_id;
 var search_results;
 var img_urls;
+
+var aside = $('aside');
 
 $.ajax({
     url: search_url,
     dataType: "jsonp",
     success: function (data) {
       search_results = data;
-      img_urls = _.map(data.data, function(d) { return d.images.standard_resolution.url; })
+      img_urls = _.map(data.data, function(d) { return d.images.standard_resolution.url; 
+       })
+      
+      for (var i = 0; i < 4; i++) {
+      var img = '<a href="#">' + 
+        '<img src=' + img_urls[i] + ' class="ig-img" />' +
+        '</a>';
+      aside.append(img);
+      }     
+ 
     }
 });
+
 
