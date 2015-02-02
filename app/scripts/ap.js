c661f24fde767588f40d06f24b6ddfebd025e03d
Alex Pate <proto.merkabah@gmail.com>
2015-02-01 20:26:24 -0500

    daily special

--- app/scripts/ap.js
+++ app/scripts/ap.js
@@ -17,7 +17,7 @@ $.ajax({
       img_urls = _.map(data.data, function(d) { return d.images.standard_resolution.url;
        })
 
-      for (var i = 0; i < 4; i++) {
+      for (var i = 0; i < 5; i++) {
         var img = '<a href="#"><div class="ig-card">' +
           '<img src=' + img_urls[i] + ' class="ig-img" />' +
           '</div></a>';
@@ -26,8 +26,8 @@ $.ajax({
 
     },
     fail: function() {
-      for (var i = 0; i < 4; i++) {
-       var img = '<div class="ig-card" "ig-fail">Unable to reach Instagram</div>';
+      for (var i = 0; i < 5; i++) {
+       var img = '<div class="ig-card ig-fail">Unable to reach Instagram</div>';
        aside.append(img);
       }
     }
@@ -68,25 +68,30 @@ setInterval( function() {
 
 
 
-/*
 // Daily Special
-var ds_endpoint = 'http://private-anon-eafb492e3-restaurantapi.apiary-mock.com/menu/special'
+var m_endpoint = 'http://private-anon-eafb492e3-restaurantapi.apiary-mock.com/menu';
+var ds_endpoint = 'http://private-anon-eafb492e3-restaurantapi.apiary-mock.com/menu/special';
 var special;
 $.getJSON(ds_endpoint, function(data) {
+  $.getJSON(m_endpoint, function(menu) {
   var id = data.menu_item_id;
-  _.each(menu, function(items) { // menu => ss.js
+  _.each(menu, function(items) { 
     _.map(items, function(item) {
       if (item.id === id) {
         special = item;
         return;
       }
-    })
-  })
+    });
+  });
 
   // Update Daily Special HTML Block
-  $('.daily-special').append(special.description.toString());
+  var daily_special = $('.daily-special');
+  var menu_template = $('#menuTemp').html();
+  var ds_template_function = _.template(menu_template);
+  daily_special.append( '<div class="ig-card"> <img class="ig-img" src="' + img_urls[10] + '" /></div>' );
+  daily_special.append(ds_template_function(special));
+  });
 });
-*/
 
 
 var res_ep = "http://tiy-atl-fe-server.herokuapp.com/collections/hipandstir"; 
@@ -104,6 +109,7 @@ $('#sub').click( function(e) {
       var reserve_template = $('#reserve-template').html().trim();
       var r_template_function = _.template(reserve_template);
       reserve_content.html(r_template_function(data));
+
     }
   });
 });
